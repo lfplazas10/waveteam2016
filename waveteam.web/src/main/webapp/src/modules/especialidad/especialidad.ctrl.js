7c17414bba8752f4b607b0971186765c6e95f474
DANIELA MARI├æO RODRIGEZ <d.marino10@uniandes.edu.co>
2016-08-30 09:34:26 -0500

    modulo especialidad sin probar

---
+++ waveteam.web/src/main/webapp/src/modules/especialidad/especialidad.ctrl.js
@@ -0,0 +1,92 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+ (function (ng) {
+    var mod = ng.module("especialidadModule");
+
+    mod.controller("especialidadCtrl", ['$scope', '$state', '$stateParams', '$http', 'especialidadContext', function ($scope, $state, $stateParams, $http, context) {
+
+            $scope.records = {};
+            $http.get(context).then(function(response){
+                $scope.records = response.data;    
+            }, responseError);
+
+
+            if ($stateParams.especialidadId !== null && $stateParams.especialidadId !== undefined) {
+                
+                id = $stateParams.especialidadId;
+                
+                $http.get(context + "/" + id)
+                    .then(function (response) {
+                        $scope.currentRecord = response.data;
+                    }, responseError);
+            } 
+            else
+            {
+                $scope.currentRecord = {
+                    id: undefined ,
+                    nombre: '',
+                    grupo: '',
+                    tipo: '',
+                };
+              
+                $scope.alerts = [];
+            }
+
+
+            this.saveRecord = function (id) {
+                currentRecord = $scope.currentRecord;
+                
+                if (id == null) {  
+                    return $http.post(context, currentRecord)
+                        .then(function () {
+                            $state.go('especialidadList');
+                        }, responseError);    
+                } 
+                else {
+                    return $http.put(context + "/" + currentRecord.id, currentRecord)
+                        .then(function () {
+                            $state.go('especialidadList');
+                        }, responseError);
+                };
+            };
+
+
+
+            // -----------------------------------------------------------------
+            // Funciones para manejra los mensajes en la aplicación
+
+
+            //Alertas
+            this.closeAlert = function (index) {
+                $scope.alerts.splice(index, 1);
+            };
+            
+            function showMessage(msg, type) {
+                var types = ["info", "danger", "warning", "success"];
+                if (types.some(function (rc) {
+                    return type === rc;
+                })) {
+                    $scope.alerts.push({type: type, msg: msg});
+                }
+            }
+
+            this.showError = function (msg) {
+                showMessage(msg, "danger");
+            };
+
+            this.showSuccess = function (msg) {
+                showMessage(msg, "success");
+            };
+
+            var self = this;
+            function responseError(response) {
+
+                self.showError(response.data);
+            }
+        }]);
+
+})(window.angular);
--- waveteam.web/src/main/webapp/src/modules/especialidad/especialidad.mod.js
+++ waveteam.web/src/main/webapp/src/modules/especialidad/especialidad.mod.js
@@ -3,10 +3,44 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+ (function (ng) {
+     
 var mod = ng.module("especialidadModule", ["ui-router"]);
 mod.config(['$stateProvider', '$urlRouterProvider', function($stateProvider,$urlRouterProvider){
     var basePath = 'src/modules/especialidad/';
-    $urlRouterProvider.otherwise("/especialidad");
-    
+    $urlRouterProvider.otherwise("/especialidadList");
+    $stateProvider.state('especialidadList', {
+                url: '/especialidad',
+                views: {
+                    'mainView': {
+                        controller: 'especialidadCtrl',
+                        controllerAs: 'ctrl',
+                        templateUrl: basePath + 'especialidad.list.html'
+                    }
+                }
+            }).state('especialidadCreate', {
+                url: '/especialidad/create',
+                views: {
+                    'mainView': {
+                        controller: 'especialidadCtrl',
+                        controllerAs: 'ctrl',
+                        templateUrl: basePath + 'especialidad.create.html'
+                    }
+                }
+
+            }).state('especialidadEdit', {
+                url: '/especialidad/:especialidadId',
+                param: {
+                    cityId: null
+                },
+                views: {
+                    'mainView': {
+                        controller: 'especialidadCtrl',
+                        controllerAs: 'ctrl',
+                        templateUrl: basePath + 'especialidad.search.html'
+                    }
+                }
+            });
 }]);
 
+})(window.angular);
