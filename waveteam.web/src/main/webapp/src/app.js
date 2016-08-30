7949388c639d95627ee501929cea8e7740c4ac25
luisplazas <lf.plazas10@uniandes.edu.co>
2016-08-29 22:16:24 -0500

    Mejor pagina

--- waveteam.web/src/main/webapp/src/app.js
+++ waveteam.web/src/main/webapp/src/app.js
@@ -11,7 +11,7 @@ app.config(function ($stateProvider, $urlRouterProvider, $compileProvider){
     
 });
 
-app.controller("appCtrl", function ($scope, $resource, $window) {
+app.controller("appCtrl", function ($scope, $http, $resource, $window) {
     
     
 });
