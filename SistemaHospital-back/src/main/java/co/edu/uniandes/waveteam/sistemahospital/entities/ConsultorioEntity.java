45f4cdeb83ffbf19d9c7d6a41937e39ba141be36
r.garcia11 <r.garcia11@ISIS2603-61.sis.virtual.uniandes.edu.co>
2016-10-06 12:15:40 -0500

    Paquetes

---
+++ SistemaHospital-back/src/main/java/co/edu/uniandes/waveteam/sistemahospital/entities/ConsultorioEntity.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.waveteam.sistemahospital.entities;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+
+/**
+ *
+ * @author r.garcia11
+ */
+@Entity
+public class ConsultorioEntity extends BaseEntity implements Serializable{
+    
+}
