bde4374b4232d4527d8c5cbe0be1be222edfac72
Nicolás Muñoz <nd.munoz10@uniandes.edu.co>
2016-10-11 09:15:48 -0500

    creacion pacienteEntity, pacientePersistence, pacientePersistenceTest

---
+++ SistemaHospital-back/src/main/java/co/edu/uniandes/waveteam/sistemahospital/entities/PacienteEntity.java
@@ -0,0 +1,92 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.waveteam.sistemahospital.entities;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.*;
+import uk.co.jemos.podam.common.PodamExclude;
+
+/**
+ *
+ * @author felipeplazas
+ */
+@Entity
+public class PacienteEntity extends BaseEntity implements Serializable{
+    
+    @PodamExclude 
+    @OneToMany(mappedBy = "paciente")
+    private List<CitaEntity> citas = new ArrayList();
+    
+    private Long id;
+    private String name;
+    private String sexo;
+    private int edad;
+    private String tipoSangre;
+    private String eps;
+
+    public List<CitaEntity> getCitas() {
+        return citas;
+    }
+
+    public void setCitas(List<CitaEntity> citas) {
+        this.citas = citas;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getTipoSangre() {
+        return tipoSangre;
+    }
+
+    public void setTipoSangre(String tipoSangre) {
+        this.tipoSangre = tipoSangre;
+    }
+
+    public String getEps() {
+        return eps;
+    }
+
+    public void setEps(String eps) {
+        this.eps = eps;
+    }
+    
+    
+
+   
+    
+}
