330bc52a982d731654375a10136b3f00278abcb2
luisplazas <lf.plazas10@uniandes.edu.co>
2016-10-11 08:24:40 -0500

    DoctorEntity first revision

---
+++ SistemaHospital-back/src/main/java/co/edu/uniandes/waveteam/sistemahospital/entities/DoctorEntity.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.waveteam.sistemahospital.entities;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.*;
+
+/**
+ *
+ * @author felipeplazas
+ */
+@Entity
+public class DoctorEntity extends BaseEntity implements Serializable{
+    
+    @OneToMany(mappedBy = "doctor", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Object> disponibilidad = new ArrayList<>();
+    
+    private Long id;
+    private String name;
+    private String especialidad;
+    private Long consultorio;
+
+    public List<Object> getDisponibilidad() {
+        return disponibilidad;
+    }
+
+    public void setDisponibilidad(List<Object> disponibilidad) {
+        this.disponibilidad = disponibilidad;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEspecialidad() {
+        return especialidad;
+    }
+
+    public void setEspecialidad(String especialidad) {
+        this.especialidad = especialidad;
+    }
+
+    public Long getConsultorio() {
+        return consultorio;
+    }
+
+    public void setConsultorio(Long consultorio) {
+        this.consultorio = consultorio;
+    }
+    
+}
