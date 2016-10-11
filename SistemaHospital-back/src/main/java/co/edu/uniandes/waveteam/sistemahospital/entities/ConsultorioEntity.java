7c0039592bc698eb0a27409bc92a23da21e85726
rgarcia11 <r.garcia11@uniandes.edu.co>
2016-10-11 07:32:20 -0500

    Metodos de ConsultorioEntity

--- SistemaHospital-back/src/main/java/co/edu/uniandes/waveteam/sistemahospital/entities/ConsultorioEntity.java
+++ SistemaHospital-back/src/main/java/co/edu/uniandes/waveteam/sistemahospital/entities/ConsultorioEntity.java
@@ -15,4 +15,60 @@ import javax.persistence.Entity;
 @Entity
 public class ConsultorioEntity extends BaseEntity implements Serializable{
     
+//    @OneToMany
+//    private List<DoctorEntity> doctoresAsignados = new ArrayList<DoctorEntity>;
+    
+//    private String nombre;
+    private String horario;
+    private boolean atencionUrgencias;
+    private boolean unidadCuidadosIntensivos;
+    
+    public String getHorario(){
+        return horario;
+    }
+    
+    public void setHorario(){
+        this.horario = horario;
+    }
+    
+    public boolean getAtencionUrgencias(){
+        return atencionUrgencias;
+    }
+    
+    public void setAtencionUrgencias(boolean atencionUrgencias){
+        this.atencionUrgencias = atencionUrgencias;
+    }
+    
+    public boolean getUnidadCuidadosintensivos(){
+        return unidadCuidadosIntensivos;
+    }
+    
+    public void setUnidadCuidadosIntensivos(){
+        this.unidadCuidadosIntensivos = unidadCuidadosIntensivos;
+    }
+    
+//    public List<DoctorEntity> getDoctoresAsignados(){
+//        return doctoresAsignados;
+//    }
+    
+//    public void setDoctoresAsignados(List<DoctorEntity> doctoresAsignados){
+//        this.doctoresAsignados = doctoresAsignados;
+//    }
+    
+//    public void agregarDoctorAsignado(DoctorEntity doc){
+//        doctoresAsignados.add(doc);
+//    }
+    
+//    public boolean eliminarDoctorAsignado(Long idDoctor)
+//    {
+//        for (DoctorEntity doc: doctoresAsignados)
+//        {
+//            if (Objects.equals(doc.getId(), idDoctor))
+//            {
+//                doctoresAsignados.remove(doc);
+//                return true;
+//            }
+//        }
+//        return false;
+//    }
 }
