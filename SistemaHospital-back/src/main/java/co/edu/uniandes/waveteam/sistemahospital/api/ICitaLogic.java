bc7c9751e77043655c859c5d4d20b16acfe9e587
miso4204-201420 <miso4204.201420@gmail.com>
2016-10-14 10:09:50 -0500

    cambio nombre artifactid proyecto a SistemaHospital

--- SistemaHospital-back/src/main/java/co/edu/uniandes/waveteam/sistemahospital/api/ICitaLogic.java
+++ SistemaHospital-back/src/main/java/co/edu/uniandes/waveteam/sistemahospital/api/ICitaLogic.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package co.edu.uniandes.waveteam.sistemahospital.logic;
+package co.edu.uniandes.waveteam.sistemahospital.api;
 
 import co.edu.uniandes.waveteam.sistemahospital.entities.CitaEntity;
 import java.util.List;
