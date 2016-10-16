a20ad38811af87622b8eb731021ac992c77d979b
rgarcia11 <r.garcia11@.uniandes.edu.co>
2016-10-16 18:25:55 -0500

    Prueba para ver si corre en Jenkins

---
+++ SistemaHospital-back/src/test/java/co/edu/uniandes/waveteam/sistemahospital/test/persistence/ConsultorioPersistenceTest.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.waveteam.sistemahospital.test.persistence;
+
+import org.junit.Assert;
+
+/**
+ *
+ * @author r.garcia11
+ */
+public class ConsultorioPersistenceTest {
+    public void getConsultoriosTest(){
+        Assert.assertTrue(true);
+    }
+}
