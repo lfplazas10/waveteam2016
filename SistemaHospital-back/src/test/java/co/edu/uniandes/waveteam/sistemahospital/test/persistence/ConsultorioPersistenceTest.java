f67dd1495be447d5fed3fe948dfe959f7034da5a
luisplazas <lf.plazas10@uniandes.edu.co>
2016-10-16 22:12:27 -0500

    Small changes

--- SistemaHospital-back/src/test/java/co/edu/uniandes/waveteam/sistemahospital/test/persistence/ConsultorioPersistenceTest.java
+++ SistemaHospital-back/src/test/java/co/edu/uniandes/waveteam/sistemahospital/test/persistence/ConsultorioPersistenceTest.java
@@ -5,13 +5,19 @@
  */
 package co.edu.uniandes.waveteam.sistemahospital.test.persistence;
 
+import org.jboss.arquillian.junit.Arquillian;
 import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 
 /**
  *
  * @author r.garcia11
  */
+@RunWith(Arquillian.class)
 public class ConsultorioPersistenceTest {
+    
+    @Test
     public void getConsultoriosTest(){
         Assert.assertTrue(true);
     }
