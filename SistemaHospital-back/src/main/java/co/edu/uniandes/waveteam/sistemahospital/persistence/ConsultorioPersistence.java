30dc155d01fec07b4695615aab933d52e90a6d22
rgarcia11 <r.garcia11@uniandes.edu.co>
2016-10-17 10:08:44 -0500

    Prueba para ver si corre en Jenkins

--- SistemaHospital-back/src/main/java/co/edu/uniandes/waveteam/sistemahospital/persistence/ConsultorioPersistence.java
+++ SistemaHospital-back/src/main/java/co/edu/uniandes/waveteam/sistemahospital/persistence/ConsultorioPersistence.java
@@ -6,15 +6,17 @@
 package co.edu.uniandes.waveteam.sistemahospital.persistence;
 
 import co.edu.uniandes.waveteam.sistemahospital.entities.ConsultorioEntity;
+import co.edu.uniandes.waveteam.sistemahospital.entities.EspecialidadEntity;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 
 /**
  *
- * @author r.garcia11
+ * @author r.garcia11eeeeeeeeeee    y6
  */
 @Stateless
 public class ConsultorioPersistence {
@@ -27,5 +29,13 @@ public class ConsultorioPersistence {
     public ConsultorioEntity find(Long id) {
         LOGGER.log(Level.INFO, "Consultando consultorio con id={0}", id);
         return em.find(ConsultorioEntity.class, id);
+    }
+    
+    public ConsultorioEntity findByName(String name){
+        LOGGER.log(Level.INFO, "Consultando consultorio con nombre={}",name);
+        TypedQuery<ConsultorioEntity> q 
+            = em.createQuery("select u from ConsultorioEntity u where u.name = :name", ConsultorioEntity.class);
+        q = q.setParameter("name", name);
+        return q.getSingleResult();
     }
 }
